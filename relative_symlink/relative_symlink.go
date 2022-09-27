80888daab2c14641e67a6c83dcf48ada225c1d25
Jeremy MATOS <jeremy.matos@grafana.com>
2022-09-27 12:49:47 +0200

    Formatting

--- relative_symlink/relative_symlink.go
+++ relative_symlink/relative_symlink.go
@@ -9,20 +9,20 @@ import (
 // isSymlinkRelativeTo checks whether symlinkDestPath is relative to basePath.
 // symlinkOrigPath is the path to file holding the symbolic link.
 func isSymlinkRelativeTo(basePath string, symlinkDestPath string, symlinkOrigPath string) bool {
-	if filepath.IsAbs(symlinkDestPath) {
-		return false
-	} else {
-		fileDir := filepath.Dir(symlinkOrigPath)
-		cleanPath := filepath.Clean(filepath.Join(fileDir, "/", symlinkDestPath))
-		p, err := filepath.Rel(basePath, cleanPath)
-		if err != nil {
-			return false
-		}
+  if filepath.IsAbs(symlinkDestPath) {
+    return false
+  } else {
+    fileDir := filepath.Dir(symlinkOrigPath)
+    cleanPath := filepath.Clean(filepath.Join(fileDir, "/", symlinkDestPath))
+    p, err := filepath.Rel(basePath, cleanPath)
+    if err != nil {
+      return false
+    }
 
     if strings.HasPrefix(p, ".."+string(filepath.Separator)) {
-			return false
-		}
-	}
+      return false
+    }
+  }
 
-	return true
+  return true
 }
